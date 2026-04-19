e5e4d89e25467a491c9637d9aa19030f3a7a5f32
padmanabh-g <41082549+padmanabh-g@users.noreply.github.com>
2026-04-19 17:37:01 +0900

    fix: force-dynamic to skip build-time prerender

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getAllData } from '@/lib/data'
 import { MapView } from '@/components/MapView'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
   const data = await getAllData()
   return <MapView data={data} />
